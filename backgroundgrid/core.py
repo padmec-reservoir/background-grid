1da58855a66bd434e728148131d0acb50adcb286
Filipe Cumaru <facsa@cin.ufpe.br>
2021-09-28 17:55:09 +0000

    refactor(core): use scipy's Delaunay to compute point in convex polyhedron

--- backgroundgrid/core.py
+++ backgroundgrid/core.py
@@ -1,7 +1,6 @@
 import numpy as np
+from scipy.spatial import Delaunay
 from preprocessor.meshHandle.finescaleMesh import FineScaleMesh
-from rtree import index
-from .utils import find_aabb_of_volumes
 
 
 class BackgroundGrid(object):
@@ -9,44 +8,21 @@ class BackgroundGrid(object):
         self.finescale_mesh = FineScaleMesh(finescale_mesh_file)
         self.bg_mesh = FineScaleMesh(bg_mesh_file)
 
-        p = index.Property()
-        p.dimension = 3
-        self.bg_volumes_tree = index.Index(properties=p)
-
     def run(self) -> None:
         pass
 
     def assign_finescale_volumes_to_bg_volumes(self) -> None:
-        # Construct bounding boxes for coarse volumes.
-        bg_volumes_vertices_handles = self.bg_mesh.volumes.connectivities[:]
-        bg_num_volumes, num_vertices_of_bg_volume = bg_volumes_vertices_handles.shape
-        bg_volumes_vertices_coords = self.bg_mesh.nodes.coords[bg_volumes_vertices_handles.flatten()].reshape(
-            (bg_num_volumes, num_vertices_of_bg_volume, 3))
-
-        bg_volumes_bboxes = find_aabb_of_volumes(bg_volumes_vertices_coords)
-        bg_volumes_ids = self.bg_mesh.volumes.all[:]
-
-        # Insert bounding boxes into R-tree.
-        [self.bg_volumes_tree.insert(volume_id, bbox) for volume_id, bbox in zip(bg_volumes_ids, bg_volumes_bboxes)]
-
-        # Find the nearest bounding box for each finescale volume.
         finescale_volumes_centroids = self.finescale_mesh.volumes.center[:]
-        nearest_bg_volumes_by_fine_volume = [
-            list(self.bg_volumes_tree.intersection(
-                (centroid[0],
-                 centroid[1],
-                 centroid[2],
-                 centroid[0],
-                 centroid[1],
-                 centroid[2]))) for centroid in finescale_volumes_centroids]
+        bg_volumes_ids = self.bg_mesh.volumes.all[:]
+        bg_volumes_connectivities = self.bg_mesh.volumes.connectivities[:]
+        bg_volumes_vertices = self.bg_mesh.nodes.coords[bg_volumes_connectivities.flatten()].reshape(
+            (bg_volumes_connectivities.shape[0], 4, 3))
+        bg_volumes_hulls = [Delaunay(vertices) for vertices in bg_volumes_vertices]
+        centroids_in_bg_volumes_check = [hull.find_simplex(
+            finescale_volumes_centroids) >= 0 for hull in bg_volumes_hulls]
 
-        i = 0
-        for centroid, nearest_bg_volumes in zip(finescale_volumes_centroids, nearest_bg_volumes_by_fine_volume):
-            D = self.bg_mesh.volumes.center[nearest_bg_volumes] - centroid
-            ds = np.linalg.norm(D, axis=1)
-            nearest_bg_volume_id = nearest_bg_volumes[ds.argmin()]
-            self.finescale_mesh.bg_volume[i] = nearest_bg_volume_id
-            i += 1
+        for centroids_in_bg_volume, bg_id in zip(centroids_in_bg_volumes_check, bg_volumes_ids):
+            self.finescale_mesh.bg_volume[centroids_in_bg_volume] = int(bg_id)
 
     def assemble_primal_volumes(self) -> None:
         pass

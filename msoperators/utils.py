6d5bcef92f0adc78b6aec5e31f31a141d239dfc8
Filipe-Cumaru <filipe.cumaru@ufpe.br>
2023-08-07 19:52:35 -0300

    fix(msoperators/utils): fix L2 error function

--- msoperators/utils.py
+++ msoperators/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def compute_ms_l2_error(u_ref, u_ms):
-    return (np.abs(u_ref - u_ms) ** 2) / (np.abs(u_ref)) ** 2
+    return np.sum(np.abs(u_ref - u_ms) ** 2) / np.sum((np.abs(u_ref)) ** 2)
 
 
 def compute_ms_inf_error(u_ref, u_ms):

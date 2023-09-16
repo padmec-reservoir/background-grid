76d3fb9b1d05c7d0161122c418632d89ed7d8b98
Filipe-Cumaru <filipe.cumaru@ufpe.br>
2023-09-16 20:46:38 -0300

    feat(operators): add the MsRSB operator

--- msoperators/operators.py
+++ msoperators/operators.py
@@ -677,3 +677,142 @@ class MsCVOperator(object):
 
         self.div = csr_matrix((d, (vols_idx, faces_idx)), shape=(
             len(self.finescale_mesh.volumes), len(self.finescale_mesh.faces)))
+
+
+class MsRSBOperator(object):
+    def __init__(self, fine_mesh, coarse_mesh, support_regions,
+                 support_boundaries, A, q):
+        self.fine_mesh = fine_mesh
+        self.coarse_mesh = coarse_mesh
+        self.support_regions = support_regions
+        self.support_boundaries = support_boundaries
+        self.A = A
+        self.q = q
+
+    def _init_mesh_data(self):
+        all_volumes = self.fine_mesh.core.all_volumes[:]
+        all_tags = self.fine_mesh.core.mb.tag_get_tags_on_entity(
+            all_volumes[0])
+
+        bg_volume_tag = [
+            tag for tag in all_tags if tag.get_name() == "bg_volume"].pop()
+
+        bg_volume_data = self.fine_mesh.core.mb.tag_get_data(bg_volume_tag,
+                                                                  all_volumes, flat=True)
+
+        self.fine_mesh.bg_volume[:] = bg_volume_data
+
+    def compute_operators(self, tol=1e-3, maxiter=50):
+        # Initialize mesh data.
+        self._init_mesh_data()
+
+        # Initialize prolongation operator with the initial guess.
+        m = len(self.fine_mesh.volumes)
+        n = len(self.coarse_mesh.volumes)
+
+        bg_volume_values = self.fine_mesh.bg_volume[:].flatten()
+        fine_vols_idx = self.fine_mesh.volumes.all[:]
+
+        dirichlet_faces_flags = self.fine_mesh.dirichlet_faces[:].flatten(
+        )
+        dirichlet_faces = self.fine_mesh.faces.all[dirichlet_faces_flags == 1]
+        dirichlet_idx = self.fine_mesh.faces.bridge_adjacencies(
+            dirichlet_faces, 2, 3).flatten()
+        dirichlet_primal_idx = self.fine_mesh.bg_volume[dirichlet_idx].flatten(
+        )
+
+        P = lil_matrix((m, n))
+        P[fine_vols_idx, bg_volume_values] = 1.0
+
+        # Set the restriction operator.
+        R = P.tocsr().transpose()
+
+        P[dirichlet_idx, dirichlet_primal_idx] = 1.0
+        P = P.tocsr()
+
+        # Initialize the increment Q of the prolongation operator.
+        omega = 2 / 3
+        D_inv = diags(1 / self.A.diagonal())
+        Q = -omega * (D_inv @ self.A)
+
+        # Get all volumes in the global support boundary.
+        all_vols_in_sup_boundary = list(
+            chain.from_iterable(self.support_boundaries.values()))
+
+        # Get all volumes in the global support boundary (G).
+        G = np.unique(all_vols_in_sup_boundary)
+        notG = ~np.isin(fine_vols_idx, G, assume_unique=True)
+
+        # Assemble some masks to filter volumes during the iterative
+        # construction of the operators.
+        # M -> all volumes in the support region
+        # H -> all volumes in the support region and in the global support boundary
+        # I -> all volumes in the support region but not in the global support boundary
+        M = lil_matrix((n, m))
+        H = lil_matrix((n, m))
+        I = lil_matrix((n, m))
+        for j in range(n):
+            S_j, B_j = self.support_regions[j], self.support_boundaries[j]
+            S_in_j = np.setdiff1d(S_j, B_j, assume_unique=True)
+            I_j = np.setdiff1d(S_j, G, assume_unique=True)
+            H_j = np.intersect1d(S_in_j, G, assume_unique=True)
+            M[j, S_in_j] = 1.0
+            H[j, H_j] = 1.0
+            I[j, I_j] = 1.0
+
+        # Remove the centres
+        xP = np.nonzero(self.fine_mesh.support_region_center[:])[0]
+        xP_idx = self.fine_mesh.bg_volume[xP].flatten()
+        M[xP_idx, xP] = 0
+        H[xP_idx, xP] = 0
+        I[xP_idx, xP] = 0
+
+        # Force the volumes belonging to a single support region
+        # to hold the maximum value of the basis function.
+        vols_in_a_single_sr = np.nonzero(M.sum(axis=0).A.ravel() == 1)[0]
+        vols_in_a_single_sr_primal_ids = self.fine_mesh.bg_volume[vols_in_a_single_sr].flatten(
+        )
+        M[vols_in_a_single_sr_primal_ids, vols_in_a_single_sr] = 0
+        H[vols_in_a_single_sr_primal_ids, vols_in_a_single_sr] = 0
+        I[vols_in_a_single_sr_primal_ids, vols_in_a_single_sr] = 0
+
+        # Convert the masks to CSC format for better performance.
+        M = M.tocsc().transpose()
+        H = H.tocsc().transpose()
+        I = I.tocsc().transpose()
+
+        # Construct the prolongation operator iteratively.
+        e = np.ones(m)
+        local_err = np.inf
+        niter = 0
+        while local_err > tol and niter < maxiter:
+            niter += 1
+
+            # Compute the initial increment.
+            d_hat = (Q @ P).multiply(M)
+
+            # Adapt the increment within the global support boundary.
+            P_H = P.multiply(H)
+            d_hat_in_H = d_hat.multiply(H)
+            Sd_H = d_hat_in_H.sum(axis=1).A.ravel()
+            d_H = (
+                d_hat_in_H - P_H.multiply(Sd_H[:, None])).multiply((1 + Sd_H[:, None]) ** -1)
+
+            # Filter the increment for the volumes outside of G.
+            d_I = d_hat.multiply(I)
+
+            # Set the final increment.
+            d = d_H + d_I
+
+            # Update the operator.
+            P = P + d
+            P_sum = diags(1 / P.sum(axis=1).A.ravel())
+            P = P_sum @ P
+
+            # Update error.
+            e = np.asarray(d[notG].max(axis=0).todense()).flatten()
+            local_err = np.linalg.norm(e, ord=np.inf)
+
+            print("It {} / Current error: {}".format(niter, local_err))
+
+        return P, R
